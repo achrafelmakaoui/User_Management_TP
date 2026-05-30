aba696e715c86fe5bf83b0ff9107e23040004a3b
Achraf EL MAKAOUI <elmakaouiachraf@gmail.com>
2026-05-30 21:22:47 +0100

    Add Get Endpoints For Users

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const userRoute = require("./routes/user");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = express();
@@ -14,6 +15,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use("/users", userRoute);
 
 app.get("/", (req, res) => {
   res.send("User Management API is running...");
---
+++ backend/routes/user.js
@@ -0,0 +1,32 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+const router = require("express").Router();
+
+
+// GET ALL USERS
+router.get("/", async (req, res) => {
+    try {
+        const users = await User.find().sort({ createdAt: -1 });
+        res.status(200).json(users);
+    } 
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET USER BY ID
+router.get("/find/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
