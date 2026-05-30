36ca1364b6cc2581a62a0c14fade562a89e67664
Achraf EL MAKAOUI <elmakaouiachraf@gmail.com>
2026-05-30 23:52:57 +0100

    Delete User Endpoint & Postman Test Screenshots

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,7 +4,6 @@ const bcrypt = require("bcryptjs");
 
 const router = express.Router()
 
-
 // GET ALL USERS
 router.get("/", async (req, res) => {
     try {
@@ -50,4 +49,21 @@ router.put("/:id", async (req, res) => {
         res.status(500).json({ message: "Error updating user", error: err.message });
     }
 });
+
+// DELETE USER
+router.delete("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+        return res.status(404).json({ message: "User not found" });
+        }
+
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "User deleted successfully" });
+    } 
+    catch (err) {
+        res.status(500).json({message: "Error deleting user", error: err.message});
+    }
+});
+
 module.exports = router;
