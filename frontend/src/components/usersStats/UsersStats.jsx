a82fa48046d51b42dac8ddd257098801a4a1c35a
Achraf EL MAKAOUI <elmakaouiachraf@gmail.com>
2026-05-31 04:50:16 +0100

    Add User State UI

--- frontend/src/components/usersStats/UsersStats.jsx
+++ frontend/src/components/usersStats/UsersStats.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import "./UsersStats.css"
 
 const usersStats = () => {
-  return (
-    <div>usersStats</div>
-  )
+    return (
+        <div className='stateBanner'>
+            <div className='stateCard'>
+                <span>Total Users</span>
+                <h3>2</h3>
+            </div>
+            <div className='stateCard'>
+                <span>Total Admin</span>
+                <h3>2</h3>
+            </div>
+            <div className='stateCard'>
+                <span>Total Regular Users</span>
+                <h3>2</h3>
+            </div>
+        </div>
+    )
 }
 
 export default usersStats
