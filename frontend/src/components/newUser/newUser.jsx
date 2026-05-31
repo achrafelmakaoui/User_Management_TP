8bbe2ceccb8dc0e5ed972146e34c5a641470e804
Achraf EL MAKAOUI <elmakaouiachraf@gmail.com>
2026-05-31 07:54:10 +0100

    Add New User & Update User Components UI

--- frontend/src/components/newUser/newUser.jsx
+++ frontend/src/components/newUser/newUser.jsx
@@ -2,9 +2,42 @@ import React from 'react'
 import './NewUser.css'
 
 const NewUser = () => {
-  return (
-    <div>newUser</div>
-  )
+    return (
+        <form className='newUserBanner'>
+            <div className='newUserContent'>
+                <h2>Add User</h2>
+                <div className='bannerForm'>
+                    <div className='formRow'>
+                        <div className='formInput'>
+                            <label>Nom Complet</label>
+                            <input type='text' placeholder='Entrer Nom complet'/>
+                        </div>
+                        <div className='formInput'>
+                            <label>Email Address</label>
+                            <input type='text' placeholder='Entrer Email Address'/>
+                        </div>
+                    </div>
+                    <div className='formRow'>
+                        <div className='formInput'>
+                            <label>Password</label>
+                            <input type='password' placeholder='Entrer Password'/>
+                        </div>
+                        <div className='formInput'>
+                            <label>Role</label>
+                            <select>
+                                <option>Select User Role</option>
+                                <option value="false">User</option>
+                                <option value="true">Admin</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div className='btnSubmit'>
+                    <button type="submit">Add User</button>
+                </div>
+            </div>
+        </form>
+    )
 }
 
 export default NewUser
--- frontend/src/components/updateUser/updateUser.jsx
+++ frontend/src/components/updateUser/updateUser.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import './UpdateUser.css'
 
 const UpdateUser = () => {
-  return (
-    <div>updateUser</div>
-  )
+    return (
+        <form className='updateUserBanner'>
+            <div className='updateUserContent'>
+                <h2>Update User</h2>
+                <div className='bannerForm'>
+                    <div className='formRow'>
+                        <div className='formInput'>
+                            <label>Nom Complet</label>
+                            <input type='text' placeholder='Entrer Nom complet'/>
+                        </div>
+                        <div className='formInput'>
+                            <label>Email Address</label>
+                            <input type='text' placeholder='Entrer Email Address'/>
+                        </div>
+                    </div>
+                    <div className='formRow'>
+                        <div className='formInput'>
+                            <label>Password</label>
+                            <input type='password' placeholder='Entrer Password'/>
+                        </div>
+                    </div>
+                </div>
+                <div className='btnSubmit'>
+                    <button type="submit">Update User</button>
+                </div>
+            </div>
+        </form>
+    )
 }
 
 export default UpdateUser
