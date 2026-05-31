d60ede58868869d5829a4352f86ee0916e734d74
Achraf EL MAKAOUI <elmakaouiachraf@gmail.com>
2026-05-31 05:31:06 +0100

    Add User Table UI

--- frontend/src/components/usersTable/UsersTable.jsx
+++ frontend/src/components/usersTable/UsersTable.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import "./UsersTable.css"
 
 const usersTable = () => {
   return (
-    <div>usersTable</div>
+    <div className='tableBanner'>
+        <table className="usersTable">
+            <thead>
+                <tr>
+                    <th>User</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Created At</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Achraf El Makoui</td>
+                    <td>elmakaouiachraf@gmail.com</td>
+                    <td className="role admin">
+                        <span>Admin</span>
+                    </td>
+                    <td>12/04/2026</td>
+                    <td>
+                      <div className='actionBtns'>
+                          <button className='actionDelete'>🗑️</button>
+                          <button className='actionUpdate'>✏️</button>
+                      </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Achraf El Makoui</td>
+                    <td>elmakaouiachraf@gmail.com</td>
+                    <td className="role user">
+                        <span>User</span>
+                    </td>
+                    <td>12/04/2026</td>
+                    <td>
+                      <div className='actionBtns'>
+                          <button className='actionDelete'>🗑️</button>
+                          <button className='actionUpdate'>✏️</button>
+                      </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
   )
 }
 
